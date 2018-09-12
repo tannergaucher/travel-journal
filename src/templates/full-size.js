c3c3a42e4bafd31987362717e71ebcf15bbc5269
tanner gaucher <tannermichaelgaucher@gmail.com>
2018-09-13 00:23:49 +0700

    remove fullsize img

--- src/templates/full-size.js
+++ src/templates/full-size.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import styled from 'styled-components'
+// import React from 'react'
+// import { graphql } from 'gatsby'
+// import Img from 'gatsby-image'
+// import styled from 'styled-components'
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-`
+// const Container = styled.div`
+//   height: 100vh;
+//   width: 100vw;
+// `
 
-export default ({ data }) => {
-  return (
-    <Container>
-      <Img
-        sizes={data.imageSharp.resolutions}
-        style={{
-          position: 'absolute',
-          top: 0,
-          width: '100%',
-          height: '100%',
-        }}
-      />
+// export default ({ data }) => {
+//   return (
+//     <Container>
+//       <Img
+//         sizes={data.imageSharp.resolutions}
+//         style={{
+//           position: 'absolute',
+//           top: 0,
+//           width: '100%',
+//           height: '100%',
+//         }}
+//       />
 
-      {/* Other aspect ratio */}
-      {/* <Img sizes={data.imageSharp.resolutions} /> */}
-    </Container>
-  )
-}
+//       {/* Other aspect ratio */}
+//       {/* <Img sizes={data.imageSharp.resolutions} /> */}
+//     </Container>
+//   )
+// }
 
 // export const query = graphql`
 //   query($slug: String!) {
